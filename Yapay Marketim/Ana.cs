973aa607a18da493b2ae4843148592467114b6f1
Sabri Kargı <sabri_16002@hotmail.com>
2021-01-03 18:55:59 +0300

    221903033

--- Yapay Marketim/Ana.cs
+++ Yapay Marketim/Ana.cs
@@ -43,9 +43,9 @@ namespace Yapay_Marketim
 
         private void toolStripButton4_Click(object sender, EventArgs e)
         {
-            Ayarlar ay = new Ayarlar();
-            ay.MdiParent = this;
-            ay.Show();
+            Ayarlar ayar = new Ayarlar();
+            ayar.MdiParent = this;
+            ayar.Show();
         }
 
         private void toolStripButton5_Click(object sender, EventArgs e)
